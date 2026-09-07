a3bc6903a504e3eb9ad8be00d1611e0326dad887
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-07 12:22:11 +0700

    feat: model business untuk workspace usaha

---
+++ app/Models/Business.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Business extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'owner_id',
+        'name',
+        'type',
+        'phone',
+        'address',
+        'logo',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function owner(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'owner_id');
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(TransactionCategory::class);
+    }
+
+    public function capitalEntries(): HasMany
+    {
+        return $this->hasMany(CapitalEntry::class);
+    }
+
+    public function operationalExpenses(): HasMany
+    {
+        return $this->hasMany(OperationalExpense::class);
+    }
+}
