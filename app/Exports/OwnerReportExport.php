7912ad6da76ad94d6cc6a699a2a73df047034adb
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-07 12:22:42 +0700

    feat: export excel laporan keuangan owner

---
+++ app/Exports/OwnerReportExport.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class OwnerReportExport implements FromCollection, WithHeadings, WithMapping, WithStyles
+{
+    protected $transactions;
+
+    protected $totalIncome;
+
+    protected $totalExpense;
+
+    protected $totalCapital;
+
+    protected $totalOperational;
+
+    protected $netProfit;
+
+    protected $periodLabel;
+
+    protected $business;
+
+    public function __construct($transactions, $totalIncome, $totalExpense, $totalCapital, $totalOperational, $netProfit, $periodLabel, $business)
+    {
+        $this->transactions = $transactions;
+        $this->totalIncome = $totalIncome;
+        $this->totalExpense = $totalExpense;
+        $this->totalCapital = $totalCapital;
+        $this->totalOperational = $totalOperational;
+        $this->netProfit = $netProfit;
+        $this->periodLabel = $periodLabel;
+        $this->business = $business;
+    }
+
+    public function collection()
+    {
+        return $this->transactions;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Tanggal',
+            'Tipe',
+            'Kategori',
+            'Sumber/Keterangan',
+            'Nominal',
+            'Pencatat',
+        ];
+    }
+
+    public function map($transaction): array
+    {
+        return [
+            $transaction->transaction_date->format('d/m/Y'),
+            $transaction->type === 'masuk' ? 'Uang Masuk' : 'Uang Keluar',
+            $transaction->category?->name ?? '-',
+            $transaction->source ?? $transaction->description ?? '-',
+            (float) $transaction->amount,
+            $transaction->user->name,
+        ];
+    }
+
+    public function styles(Worksheet $sheet): array
+    {
+        return [
+            1 => ['font' => ['bold' => true]],
+        ];
+    }
+}
